6a018e36610f98b9f2711e38e8ffd93414ef6428
Robert Giessmann <robert.giessmann@bayer.com>
2021-05-20 15:45:43 +0200

    remove help/myst.md from error messages

--- scripts/extract-warnings.sh
+++ scripts/extract-warnings.sh
@@ -1,4 +1,5 @@
 cat _build/cleaned_build.log | 
 grep "WARNING" | 
 grep -vE "\.md:: WARNING: No footnote definitions found for label: '(.*)' \[myst\.footnote\]" |
-grep -vE "WARNING: image file not readable: (.*)\.mmd\.png"
+grep -vE "WARNING: image file not readable: (.*)\.mmd\.png" |
+grep -v "/content/recipes/help/myst.md:"
