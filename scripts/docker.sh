cf7f542e485488363e3118eff63f614832e0ab42
Robert Giessmann <robert.giessmann@bayer.com>
2021-05-21 09:54:05 +0200

    adds numbering to interop chapter

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
+
+## a total fresh run needs first (decide on your needs!): 
+# rm -rf _build
+## continue with this:
 BUILDKIT_PROGRESS=plain docker build -t fcb . && docker run --name asdf fcb && docker cp asdf:/out.tar.gz . && docker rm asdf && tar -xzf out.tar.gz && rm out.tar.gz 
