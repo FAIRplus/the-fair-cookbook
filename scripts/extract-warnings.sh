574a60034250cc13b25d272a14e61807d58b5ef3
Robert Giessmann <robert.giessmann@bayer.com>
2021-11-30 14:11:13 +0100

    cicd

--- scripts/extract-warnings.sh
+++ scripts/extract-warnings.sh
@@ -3,6 +3,7 @@ grep "WARNING" |
 grep -vE "\.md:: WARNING: No footnote definitions found for label: '(.*)' \[myst\.footnote\]" |
 grep -vE "WARNING: image file not readable: (.*)\.mmd\.png" |
 grep -v "/content/recipes/help/myst.md:" |
-grep -v "/content/recipes/help/myst.md.rst:" ||
-true 
-## script must end with || true, because grep gives exit code 1 if no line found (== no errors)
+grep -v "/content/recipes/help/myst.md.rst:" |
+grep "WARNING"
+test $? -eq 1
+## grep gives exit code 1 if no line found (== no errors)
