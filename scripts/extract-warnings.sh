c3b8f4015118d80fa46d2a1a313a830886d4e1b8
Robert Giessmann <robert.giessmann@bayer.com>
2022-03-19 20:19:32 +0100

    extract-warnings script: fixes True -> true mistake

--- scripts/extract-warnings.sh
+++ scripts/extract-warnings.sh
@@ -4,4 +4,4 @@ grep -vE "\.md:: WARNING: No footnote definitions found for label: '(.*)' \[myst
 grep -vE "WARNING: image file not readable: (.*)\.mmd\.png" |
 grep -v "/content/recipes/help/myst.md:" |
 grep -v "/content/recipes/help/myst.md.rst:" |
-grep "WARNING" || True
+grep "WARNING" || true
