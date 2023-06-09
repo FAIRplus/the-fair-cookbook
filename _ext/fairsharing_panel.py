bd4af7af1c1d133901c311fd34edda934a4369ee
Ramon Granell <ragrator@gmail.com>
2023-06-09 11:37:11 +0100

    Sorting links in the FAIRsharing panel

--- _ext/fairsharing_panel.py
+++ _ext/fairsharing_panel.py
@@ -44,7 +44,8 @@ class FAIRsharingPanel(Directive):
         """
         if self.fcb_FAIRsharing_links.items():
             output = '<ul>'
-            for FAIRsharing_title, FAIRsharing_id in self.fcb_FAIRsharing_links.items():
+            aux = dict(sorted(self.fcb_FAIRsharing_links.items()))
+            for FAIRsharing_title, FAIRsharing_id in aux:
                 output += f'<li><a href="{FAIRsharing_id}" target="_blank">{FAIRsharing_title}</a></li>'
             output += '</ul>'
             return output
